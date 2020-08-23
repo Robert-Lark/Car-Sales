0de2fe6f51f98672539ee661159813d5472f7922
Rob Lark <quietcalmrecords@Robs-iMac.local>
2020-08-23 10:13:03 -0700

    about to start project

---
+++ src/REDUCER/Reducers/index.js
@@ -0,0 +1,23 @@
+// import redux from 'redux';
+// import { Actions} from "../Actions";
+
+
+// export const initialState = {
+//     key0: "original value",
+//     key1: "original value",
+// }
+
+// export const reducer = (state = initialState, action) => {
+// switch(action.type) {
+//     case DO_SOMETHING: return {
+//         ...state,
+//         key0: "change in value",
+//     }
+
+//     case DO_SOMETHING_ELSE: return {
+//         ...state,
+//         key1: "change in value",
+//     }
+//     default: return state
+// }
+// }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import {reducer, initialState} from './REDUCER/Reducers'
+import redux from 'redux';
 import ReactDOM from 'react-dom';
-import App from './App';
-
+import App from './REDUCER/App';
 import 'bulma/css/bulma.css';
 import './styles.scss';
+
+// const store = createStore(reducer)
+// console.log("initialState", store.getState() )
+// const unsubscribe = store.subscribe(() => console.log('updated state', store.getState()))
+// store.dispatch(doSomething())
+// unsubscribe()
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(<App />, rootElement);
